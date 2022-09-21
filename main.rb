d563cc209a01f9d96bfb26c85373fc637550acbc
Said-laasri <100702612+Said-laasri@users.noreply.github.com>
2022-09-21 14:00:47 +0100

    Add decode char and word to main.ruby file

--- main.rb
+++ main.rb
@@ -1,4 +1,43 @@
-def code(string)
-  puts("hello #{string}")
+def decode_char(char)
+character = { '.-' => 'A',
+            '-...' => 'B',
+            '-.-.' => 'C',
+            '-..' => 'D',
+            '.' => 'E',
+            '..-.' => 'F',
+            '--.' => 'G',
+            '....' => 'H',
+            '..' => 'I',
+            '.---' => 'J',
+            '-.-' => 'K',
+            '.-..' => 'L',
+            '--' => 'M',
+            '-.' => 'N',
+            '---' => 'O',
+            '.--.' => 'P',
+            '--.-' => 'Q',
+            '.-.' => 'R',
+            '...' => 'S',
+            '-' => 'T',
+            '..-' => 'U',
+            '...-' => 'V',
+            '.--' => 'W',
+            '-..-' => 'X',
+            '-.--' => 'Y',
+            '--..' => 'Z' }
+            character[char]
 end
-code('onyin')
+
+def decode_word(word)
+  code = word.split(/ /)
+  word = ''
+  code.each do |serial|
+    word = "#{word}#{decode_char(serial)}"
+  end
+  word
+end
+
+
+
+puts decode_intel('-- -.-- -. .- -- .')
+puts decode_intel('.- -... --- -..- ..-. ..- .-.. .-.. --- ..-. .-. ..- -... .. . ...')
